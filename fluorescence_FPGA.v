57f3b97ef3a9a512fe5951856f20e5db77b38d45
BasedOnTechnology <therobotist@gmail.com>
2020-11-06 00:17:01 -0500

    Functional FPGA counter

--- fluorescence_FPGA.v
+++ fluorescence_FPGA.v
@@ -8,13 +8,38 @@ module fluorescence_FPGA(PMT_in, light_source_pin, clock_50_mhz, pulse_out_pin, 
 	reg [31:0] light_modulation_timer = 0;	
 
 	reg [31:0] pulse_count = 0;	
+	
 //
-//   reg [31:0] add_count = 0;	
-//	reg [31:0] subtract_count = 0;
+   reg [31:0] add_count = 0;	
+	reg [31:0] subtract_count = 0;
 	
 	reg [31:0] pulse_out_accumulator= 0;
 	
+	reg [31:0] pulse_subtracted_signed_value= 0;
+	
 	output [7:0] LEDs;
+	
+	reg [31:0] blank2 = 0;
+
+	reg [31:0] blank = 0;
+
+	reg s_ena = 0;
+	reg s_clk = 0;
+	
+	altsource_probe_top #(
+		.sld_auto_instance_index ("YES"),
+		.sld_instance_index      (0),
+		.instance_id             ("cnt"),
+		.probe_width             (32),
+		.source_width            (32),
+		.source_initial_value    ("0"),
+		.enable_metastability    ("YES")
+	) in_system_sources_probes_0 (
+		.source     (blank2),     //    sources.source
+		.source_ena (s_clk), //           .source_ena
+		.source_clk (s_ena), // source_clk.clk
+		.probe      (pulse_subtracted_signed_value)       //     probes.probe
+	);
 	
 	input PMT_in; //GPIO_01 PIN_C3
 	input clock_50_mhz; //CLOCK_50 PIN_R8
@@ -95,21 +120,18 @@ module fluorescence_FPGA(PMT_in, light_source_pin, clock_50_mhz, pulse_out_pin, 
 		if(integration_timer >= integration_time-1)
 		begin 
 			integration_timer <= 32'd0;
-//			if(add_count >= subtract_count)
-//			begin
-//			pulse_out_accumulator<= add_count;
-//			subtract_out_accumulator<= add_count;
-			pulse_out_accumulator <= pulse_count;
-//			end
-//			else
-//			begin
-//				pulse_out_accumulator <= 0;
-//			end
-//			
-//			add_count <= 0;
-//			subtract_count <= 0;
-
-			pulse_count <= integration_time/2;
+			pulse_subtracted_signed_value <= $signed(add_count) - $signed(subtract_count);
+			if(add_count >= subtract_count)
+			begin
+				pulse_out_accumulator<= add_count - subtract_count;
+			end
+			else
+			begin
+				pulse_out_accumulator <= 0;
+			end
+			
+			add_count <= 0;
+			subtract_count <= 0;
 		end
 		else
 		begin
@@ -136,16 +158,10 @@ module fluorescence_FPGA(PMT_in, light_source_pin, clock_50_mhz, pulse_out_pin, 
 		
 		
 		pulse_out <= (pulse_out_accumulator > 0);
-//		pulse_out <= (pulse_out_accumulator > 0);
 
 		if(pulse_out_accumulator > 0)
 		begin
 			pulse_out_accumulator <= pulse_out_accumulator - 1;
-		end
-		
-		if(pulse_out_accumulator > 0)
-		begin
-			subtract_out_accumulator <= pulse_out_accumulator - 1;
 		end
 		
 	end
