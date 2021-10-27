fa499394fdad953c06db550d5ee4093d27fccd4a
BasedOnTechnology <therobotist@gmail.com>
2021-10-27 11:18:56 -0400

    Moved again

--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 #pandoc ../../documents/fluroescence.md -o fluorescence.html
-pandoc --filter pandoc-citeproc --bibliography=../../documents/references.bib -s ../../documents/fluorescence.tex -o paper.md
+pandoc --filter pandoc-citeproc --bibliography=../../documents/references.bib -s ../../documents/fluorescence.tex -o README.md
+pandoc --filter pandoc-citeproc --bibliography=../../documents/references.bib -s ../../documents/fluorescence.tex -o paper.pdf
